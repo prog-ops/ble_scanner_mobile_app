da9dc8428f5d18c32d79356b7a7fa2d3144f7835
prog-ops <akunbaru14102019@gmail.com>
2024-05-19 14:09:10 +0700

    Custom modal component

---
+++ apps/components/CustomModal.tsx
@@ -0,0 +1,125 @@
+import React, {FC, useCallback} from 'react';
+import {
+  FlatList,
+  ListRenderItemInfo,
+  Modal,
+  SafeAreaView,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import {Device} from 'react-native-ble-plx';
+import {MyText} from './MyText';
+
+type PeripheralModalListItemProps = {
+  item: ListRenderItemInfo<Device>;
+  connectToPeripheral: (device: Device) => void;
+  closeModal: () => void;
+};
+
+type PeripheralModalProps = {
+  devices: Device[];
+  visible: boolean;
+  connectToPeripheral: (device: Device) => void;
+  closeModal: () => void;
+};
+
+const PeripheralModalListItem: FC<PeripheralModalListItemProps> = props => {
+  const {item, connectToPeripheral, closeModal} = props;
+
+  const connectAndCloseModal = useCallback(() => {
+    connectToPeripheral(item.item);
+    closeModal();
+  }, [closeModal, connectToPeripheral, item.item]);
+
+  return (
+    <TouchableOpacity
+      onPress={connectAndCloseModal}
+      style={modalStyle.ctaButton}>
+      <Text style={modalStyle.ctaButtonText}>{item.item.name}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const PeripheralModal: FC<PeripheralModalProps> = props => {
+  const {devices, visible, connectToPeripheral, closeModal} = props;
+
+  const renderDeviceModalListItem = useCallback(
+    (item: ListRenderItemInfo<Device>) => {
+      return (
+        <PeripheralModalListItem
+          item={item}
+          connectToPeripheral={connectToPeripheral}
+          closeModal={closeModal}
+        />
+      );
+    },
+    [closeModal, connectToPeripheral],
+  );
+
+  return (
+    <Modal
+      style={modalStyle.modalContainer}
+      animationType="slide"
+      transparent={false}
+      visible={visible}>
+      <SafeAreaView style={modalStyle.modalTitle}>
+        <MyText
+          style={[modalStyle.modalTitleText, {color: 'black'}]}
+          children={'Tap on a device to connect'}
+        />
+        <FlatList
+          contentContainerStyle={modalStyle.modalFlatlistContiner}
+          data={devices}
+          renderItem={renderDeviceModalListItem}
+        />
+      </SafeAreaView>
+    </Modal>
+  );
+};
+
+const modalStyle = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  modalFlatlistContiner: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  modalCellOutline: {
+    borderWidth: 1,
+    borderColor: 'black',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    paddingVertical: 15,
+    borderRadius: 8,
+  },
+  modalTitle: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+  },
+  modalTitleText: {
+    marginTop: 40,
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginHorizontal: 20,
+    textAlign: 'center',
+  },
+  ctaButton: {
+    backgroundColor: 'purple',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    marginHorizontal: 20,
+    marginBottom: 5,
+    borderRadius: 8,
+  },
+  ctaButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
+
+export default PeripheralModal;
